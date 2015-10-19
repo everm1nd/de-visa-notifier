2eecc7a39901c5ff8c6660510dd09e279638eb95
everm1nd <everm1nd.mail@gmail.com>
2015-10-19 23:39:13 +0300

    catch exception when email can not be send added --email option to set email from CLI

--- notifier.rb
+++ notifier.rb
@@ -1,12 +1,36 @@
 require 'thor'
 require 'pdf-reader'
+require 'mail'
 
 class Notifier < Thor
+  option :email
+
   desc 'search BARCODE', 'search BARCODE in list'
   def search(barcode)
     reader = PDF::Reader.new("test/input.pdf")
     reader.pages.each do |page|
-      puts 'found!' if page.text.match barcode
+      notify(barcode) if page.text.match barcode
+    end
+  end
+
+  private
+  def notify(barcode)
+    mail(barcode) if options[:email]
+    puts "Found barcode: #{barcode}"
+  end
+
+  def mail(barcode)
+    email = options[:email]
+    begin
+      Mail.deliver do
+        from     'notifier@everm1nd.dev'
+        to       email
+        subject  'Yay! Your visa is ready!'
+        body     ':) :) :)'
+        add_file './media/attach.jpg'
+      end
+    rescue
+      puts '[ERROR]: Failed to send email'
     end
   end
 end
